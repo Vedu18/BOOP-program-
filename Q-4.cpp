bbecca15c948f329f09ebb1887ea9250b895793a
Chaudhari Vedant <129516221+Vedu18@users.noreply.github.com>
2023-04-05 21:40:26 +0530

    Add files via upload

---
+++ Q-4.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a,b,temp;
+
+    cout << "Type a number: ";
+    cin >> a;
+    
+    cout << "Type another number: ";
+    cin >> b;
+
+
+    cout << "Before swapping." << endl;
+    cout << "a = " << a << ", b = " << b << endl;
+
+    temp = a;
+    a = b;
+    b = temp;
+
+    cout << "\nAfter swapping." << endl;
+    cout << "a = " << a << ", b = " << b << endl;
+
+    return 0;
+}
